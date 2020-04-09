3ff495233bf58e0e6223da3262eeadf551e815aa
Oleksandr Rudenko <s19434@pjwstk.edu.pl>
2020-04-09 04:44:26 +0200

    FINAL

---
+++ CW4/SQLQuery1.sql
@@ -0,0 +1,68 @@
+﻿Create Procedure PromoteStudentS @Studies Varchar(100), @Semester INT
+AS
+
+
+    BEGIN
+
+        DECLARE @IdStudies INT
+        DECLARE @IdEnrollment INT
+
+        Select @IdStudies = IdStudy
+        FROM Studies WHERE Name = @Studies;
+
+     IF @IdStudies IS NULL
+         
+         BEGIN
+             RAISERROR ('ERROR this record does not exist', 1, 2);
+
+	    RETURN;
+
+      END
+
+        Select @IdEnrollment = IdEnrollment
+
+                 FROM Enrollment WHERE Semester = @Semester + 1 AND IdStudy = @IdStudies;
+
+        IF @IdEnrollment IS NULL
+
+         BEGIN
+
+                DECLARE @NewIdEnrollment INT = (Select NULLIF(MAX(IdEnrollment), 0) from Enrollment) + 1;
+                
+                INSERT INTO Enrollment VALUES(@NewIdEnrollment, @Semester + 1, @IdStudies, GETDATE());
+                
+                SET @IdEnrollment = (SELECT IdEnrollment FROM Enrollment WHERE Semester = @Semester + 1 AND IdStudy = @IdStudies);
+
+         END
+
+
+     DECLARE PromoteStudent_cursor CURSOR
+        
+        FOR SELECT IndexNumber FROM Student INNER JOIN Enrollment ON Enrollment.IdEnrollment = Student.IdEnrollment WHERE Semester = @Semester AND IdStudy = @IdStudies
+
+    OPEN MY_Cursor;
+
+        DECLARE @IndexNumber VARCHAR(150);
+
+        FETCH NEXT FROM MY_Cursor INTO @IndexNumber;
+
+
+    WHILE @@FETCH_STATUS = 0
+        
+    BEGIN
+
+       UPDATE Student SET IdEnrollment = @IdEnrollment WHERE IndexNumber = @IndexNumber;
+
+            FETCH NEXT FROM MY_Cursor
+
+       INTO @IndexNumber;
+       
+       END
+
+
+
+
+    CLOSE MY_Cursor;
+    END;
+
+  
---
+++ CW4/Services/IStudentsDbService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using CW4.DTOs.Requests;
+using CW4.Models;
+
+namespace CW4.Services
+{
+    public interface IStudentsDbService
+    {
+        public IEnumerable<Student> GetStudents();
+        public IEnumerable<Enrollment> GetEnrollments(string index);
+        public Enrollment EnrollStudent(EnrollStudentRequest request);
+        public Enrollment PromoteStudent(PromoteStudentRequest request);
+    }
+}
